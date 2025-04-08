e1587aac5b9544c7bd5386576e724183b30f7273
thales3006 <thales3006@gmail.com>
2025-04-08 14:01:08 -0300

    bigger tittles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-xl">Welcome</h1>
+      <h1 className="font-bold text-4xl">Welcome</h1>
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <Link href="/calc">Calculate Panizio Coeficient</Link>
       </main>
